e9ad2f7d9e05bbde697afdb3ac178d9d68369445
Marcos Silva <tio.makin@gmail.com>
2022-03-06 16:52:16 +0100

    feat: unit tests

--- src/users/users.resolver.spec.ts
+++ src/users/users.resolver.spec.ts
@@ -1,13 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import * as Chance from 'chance';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './entities/user.entity';
+import * as mongoose from 'mongoose';
+import { ListUsersInput } from './dto/list-users.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+const USER_ROLE = 'User';
+const chance = new Chance();
+const createUserInput: CreateUserInput = {
+  firstName: chance.first(),
+  lastName: chance.last(),
+  email: chance.email(),
+  role: USER_ROLE,
+};
+const userId = new mongoose.Types.ObjectId();
+const updateUserInput: UpdateUserInput = {
+  _id: userId.toString(),
+  lastName: chance.last(),
+  firstName: chance.first(),
+};
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
+      providers: [
+        UsersResolver,
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest.fn(() => {
+              return {
+                _id: userId,
+                ...createUserInput,
+              };
+            }),
+            findAll: jest.fn(() => {
+              return [
+                {
+                  _id: userId,
+                  ...createUserInput,
+                },
+              ];
+            }),
+            getUsers: jest.fn(() => {
+              return {
+                users: [
+                  {
+                    _id: userId,
+                    ...createUserInput,
+                  },
+                ],
+                count: 1,
+              };
+            }),
+            findOne: jest.fn(() => {
+              return {
+                _id: userId,
+                ...createUserInput,
+              };
+            }),
+            update: jest.fn(() => {
+              return {
+                _id: userId,
+                ...createUserInput,
+                ...updateUserInput,
+              };
+            }),
+            remove: jest.fn(() => {
+              return {};
+            }),
+          },
+        },
+      ],
     }).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
@@ -15,5 +84,46 @@ describe('UsersResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  it('should be able to create an user', async () => {
+    const user = await resolver.createUser(createUserInput);
+    expect(user._id).toBeDefined();
+    expect(user._id).toBe(userId);
+    expect(user.firstName).toBe(createUserInput.firstName);
+    expect(user.lastName).toBe(createUserInput.lastName);
+    expect(user.email).toBe(createUserInput.email);
+    expect(user.role).toBe(createUserInput.role);
+  });
+  it('should be able to list all users', async () => {
+    const listUserInput: ListUsersInput = { limit: 10, offset: 0 };
+    const users = await resolver.findAll(listUserInput);
+    expect(users).toBeDefined();
+    expect(Array.isArray(users)).toBe(true);
+    expect(users[0]._id).toBe(userId);
+  });
+  it('should be able to list users with cursor', async () => {
+    const findAllArgs = { first: 2 };
+    const usersWithCursor = await resolver.findAllWithCursor(findAllArgs);
+    expect(usersWithCursor.page.edges).toBeDefined();
+    expect(usersWithCursor.page.edges[0].node).toBeDefined();
+    expect(usersWithCursor.page.edges[0].node.firstName).toBe(
+      createUserInput.firstName,
+    );
+  });
+  it('should be able to find one user by id', async () => {
+    const user = await resolver.findOne(userId.toString());
+    expect(user).toBeDefined();
+    expect(user._id).toBe(userId);
+  });
+  it('should be able to test updateUser ', async () => {
+    const updatedUser = await resolver.updateUser(updateUserInput);
+    expect(updatedUser).toBeDefined();
+    expect(updatedUser.firstName).toBe(updateUserInput.firstName);
+    expect(updatedUser.lastName).toBe(updateUserInput.lastName);
+  });
+  it('should be able to test removeUser ', async () => {
+    const removedUser = await resolver.removeUser(userId.toString());
+    expect(removedUser).toBeDefined();
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,132 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CreateUserInput } from './dto/create-user.input';
+import * as Chance from 'chance';
+import { User, UserSchema } from './entities/user.entity';
+import {
+  closeInMongodConnection,
+  rootMongooseTestModule,
+} from '../common/helpers/mongoose.helper';
+import { ListUsersInput } from './dto/list-users.input';
+import { UpdateUserInput } from './dto/update-user.input';
+
+const USER_ROLE = 'User';
+const chance = new Chance();
+const createUserInput: CreateUserInput = {
+  firstName: chance.first(),
+  lastName: chance.last(),
+  email: chance.email(),
+  role: USER_ROLE,
+};
+let userId = '';
+
+const updateUserInput: UpdateUserInput = {
+  _id: userId,
+  lastName: chance.last(),
+  firstName: chance.first(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
+  let module: TestingModule;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        rootMongooseTestModule(),
+        MongooseModule.forFeature([
+          {
+            name: User.name,
+            schema: UserSchema,
+          },
+        ]),
+      ],
       providers: [UsersService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
+  afterAll(async () => {
+    if (module) {
+      await module.close();
+      await closeInMongodConnection();
+    }
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create an user with createUserInput', async () => {
+    const user = await service.create(createUserInput);
+    expect(user.id).toBeDefined();
+    expect(user.firstName).toBe(createUserInput.firstName);
+    expect(user.lastName).toBe(createUserInput.lastName);
+    expect(user.email).toBe(createUserInput.email);
+    expect(user.role).toBe(createUserInput.role);
+    userId = user.id;
+  });
+
+  it('should get a list of users', async () => {
+    const paginationQuery: ListUsersInput = { offset: 0, limit: 10 };
+    const { users, count } = await service.getUsers(paginationQuery);
+    expect(users).toBeDefined();
+    expect(count).toBeDefined();
+    expect(count).toBe(1);
+    expect(Array.isArray(users)).toBe(true);
+    expect(users.length).toBe(1);
+    expect(users[0].firstName).toBe(createUserInput.firstName);
+    expect(users[0].lastName).toBe(createUserInput.lastName);
+    expect(users[0].email).toBe(createUserInput.email);
+    expect(users[0].role).toBe(createUserInput.role);
+  });
+
+  it('should get the user by its own userId', async () => {
+    const user = await service.findOne(userId);
+    expect(user.id).toBe(userId);
+    expect(user.firstName).toBe(createUserInput.firstName);
+    expect(user.lastName).toBe(createUserInput.lastName);
+    expect(user.email).toBe(createUserInput.email);
+    expect(user.role).toBe(createUserInput.role);
+  });
+
+  it('should update some user properties', async () => {
+    updateUserInput._id = userId;
+    const updatedUser = await service.update(
+      updateUserInput._id,
+      updateUserInput,
+    );
+    expect(updatedUser.id).toBe(userId);
+    expect(updatedUser.firstName).toBe(updateUserInput.firstName);
+    expect(updatedUser.firstName).not.toBe(createUserInput.firstName);
+    expect(updatedUser.lastName).toBe(updateUserInput.lastName);
+    expect(updatedUser.lastName).not.toBe(createUserInput.lastName);
+  });
+
+  it('should delete the testing user', async () => {
+    const deletedUser = await service.remove(userId);
+    expect(deletedUser).toBeDefined();
+  });
+
+  it('should receive not found error for getting the deleted user', async () => {
+    try {
+      await service.findOne(userId);
+    } catch (err) {
+      expect(err).toBeDefined();
+      expect(err.response).toBeDefined();
+      expect(err.response.statusCode).toBe(404);
+    }
+  });
+
+  it('should not be able to update an non existing user', async () => {
+    try {
+      await service.update(userId, updateUserInput);
+    } catch (err) {
+      expect(err).toBeDefined();
+      expect(err.response).toBeDefined();
+      expect(err.response.statusCode).toBe(404);
+    }
   });
 });
